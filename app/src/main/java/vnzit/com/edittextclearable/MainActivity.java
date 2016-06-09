4eebbe458b5769b033ba37e312a5082ee0f0226b
Duong Quang Son <quangson91@gmail.com>
2016-06-09 18:30:51 +0700

    add sample use edittext clearable

--- app/src/main/java/vnzit/com/edittextclearable/MainActivity.java
+++ app/src/main/java/vnzit/com/edittextclearable/MainActivity.java
@@ -2,12 +2,28 @@ package vnzit.com.edittextclearable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ClearableEditText clearableEditText = (ClearableEditText) findViewById(R.id.clearableEditText);
+        clearableEditText.setCallback(new ClearableEditText.Callback() {
+            @Override
+            public void beforeClear(EditText editText) {
+                Log.i(TAG, "Before clear: " + editText.getText());
+            }
+
+            @Override
+            public void afterClear(EditText editText) {
+                Log.i(TAG, "After clear: " + editText.getText());
+            }
+        });
     }
 }
